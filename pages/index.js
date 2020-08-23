2757b2204402a960411159cd9486b8e4850b4004
David May <me@davidmay.de>
2020-08-23 22:42:18 +0200

    initial commit nextjs starter

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -10,8 +11,9 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Learn <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        Read <Link href="/posts/first-post"><a>this page!</a></Link>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
